152809975ba9bf7d0c64b6b2155ec8262e6a22f0
Magno <magno.alefb@gmail.com>
2022-09-13 21:42:38 -0300

    estudando a noite

--- App.js
+++ App.js
@@ -6,137 +6,137 @@
  * @flow strict-local
  */
 
-import React, { useState } from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  TextInput,
-  useColorScheme,
-  View,
-} from 'react-native';
+// import React, { useState } from 'react';
+// import type {Node} from 'react';
+// import {
+//   SafeAreaView,
+//   ScrollView,
+//   StatusBar,
+//   StyleSheet,
+//   Text,
+//   TextInput,
+//   useColorScheme,
+//   View,
+// } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+// import {
+//   Colors,
+//   DebugInstructions,
+//   Header,
+//   LearnMoreLinks,
+//   ReloadInstructions,
+// } from 'react-native/Libraries/NewAppScreen';
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+// const Section = ({children, title}): Node => {
+//   const isDarkMode = useColorScheme() === 'dark';
+//   return (
+//     <View style={styles.sectionContainer}>
+//       <Text
+//         style={[
+//           styles.sectionTitle,
+//           {
+//             color: isDarkMode ? Colors.white : Colors.black,
+//           },
+//         ]}>
+//         {title}
+//       </Text>
+//       <Text
+//         style={[
+//           styles.sectionDescription,
+//           {
+//             color: isDarkMode ? Colors.light : Colors.dark,
+//           },
+//         ]}>
+//         {children}
+//       </Text>
+//     </View>
+//   );
+// };
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+// const App: () => Node = () => {
+//   const isDarkMode = useColorScheme() === 'dark';
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-  const [username,setUsername] = useState ('')
-  const [email,setemail] = useState('')
-  const [password,setpassword] = useState('')
-  //console.log('teste',username)
-  function handleSignIn(){
-   const data = { username,
-    email,
-    password }
-    console.log(data)
-  }
+//   const backgroundStyle = {
+//     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+//   };
+//   const [username,setUsername] = useState ('')
+//   const [email,setemail] = useState('')
+//   const [password,setpassword] = useState('')
+//   //console.log('teste',username)
+//   function handleSignIn(){
+//    const data = { username,
+//     email,
+//     password }
+//     console.log(data)
+//   }
 
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <View>
-        <Text>{}</Text>
-        <Text>Magno barroso</Text>
-        <TextInput
-        onChangeText={setUsername}
-        value= {username}
-        placeholder='seu nome'
-        />
-        <TextInput
-        onChangeText={setemail}
-        value= {email}
-        placeholder='email'
-        />
-        <TextInput
-        onChangeText={setpassword}
-        value= {password}
-        placeholder='senha'
-        />
-      </View>
-      {/* <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Hello,word!!!">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView> */}
-    </SafeAreaView>
-  );
-};
+//   return (
+//     <SafeAreaView style={backgroundStyle}>
+//       <View>
+//         <Text>{}</Text>
+//         <Text>Magno barroso</Text>
+//         <TextInput
+//         onChangeText={setUsername}
+//         value= {username}
+//         placeholder='seu nome'
+//         />
+//         <TextInput
+//         onChangeText={setemail}
+//         value= {email}
+//         placeholder='email'
+//         />
+//         <TextInput
+//         onChangeText={setpassword}
+//         value= {password}
+//         placeholder='senha'
+//         />
+//       </View>
+//       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+//       <ScrollView
+//         contentInsetAdjustmentBehavior="automatic"
+//         style={backgroundStyle}>
+//         <Header />
+//         <View
+//           style={{
+//             backgroundColor: isDarkMode ? Colors.black : Colors.white,
+//           }}>
+//           <Section title="Hello,word!!!">
+//             Edit <Text style={styles.highlight}>App.js</Text> to change this
+//             screen and then come back to see your edits.
+//           </Section>
+//           <Section title="See Your Changes">
+//             <ReloadInstructions />
+//           </Section>
+//           <Section title="Debug">
+//             <DebugInstructions />
+//           </Section>
+//           <Section title="Learn More">
+//             Read the docs to discover what to do next:
+//           </Section>
+//           <LearnMoreLinks />
+//         </View>
+//       </ScrollView>
+//     </SafeAreaView>
+//   );
+// };
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+// const styles = StyleSheet.create({
+//   sectionContainer: {
+//     marginTop: 32,
+//     paddingHorizontal: 24,
+//   },
+//   sectionTitle: {
+//     fontSize: 24,
+//     fontWeight: '600',
+//   },
+//   sectionDescription: {
+//     marginTop: 8,
+//     fontSize: 18,
+//     fontWeight: '400',
+//   },
+//   highlight: {
+//     fontWeight: '700',
+//   },
+// });
 
-export default App;
+// export default App;
